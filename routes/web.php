d086ae74281213f1f393ba4f2366818bd9be393a
kalys <61675998+kalyskhairy@users.noreply.github.com>
2020-09-17 09:21:44 +0700

    learning

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-
-    return view('home');
-
-});
+Route::get('/', 'HomeController@index');
 Route::view('/about', 'about');
